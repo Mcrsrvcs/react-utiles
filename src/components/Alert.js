ba4d8e584b457add1bdbe2b7936399c4cdc34a9b
mcrsrvcs <mcrsrvcs@gmail.com>
2022-02-03 16:27:34 +0530

    React_Utiles_v1_03_02_2022

---
+++ src/components/Alert.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function alert(props) {
+  return (
+      props.alert && <div>
+      <div className={`alert alert-${props.alert.type} alert-dismissible fade show role="alert"`}>
+  <strong className="text-capitalize">{props.alert.type}</strong> {props.alert.msg}
+  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+    </div>
+  )
+}
